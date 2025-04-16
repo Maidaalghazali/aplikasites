345886f074220636621df8e431bb03c46f5fed3e
alghazali <h1101231059@student.untan.ac.id>
2025-04-16 16:35:41 +0700

    update-home2
    update-home2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ini Halaman Home</title>
 </head>
-<body>
 
+<body>
+    <h1> Ini Halaman Home</h1>
+    <p> perubahan</p>
 </body>
+
 </html>
